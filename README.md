fb0b00efebc29024358b0e0c56797428d4cbf2f9
Yael Ben Shalom <yaelbenshalom@u.northwestern.edu>
2020-10-10 20:27:18 -0500

    python package passed tests! :)

--- README.md
+++ README.md
@@ -14,8 +14,6 @@ This package contains files that control the turtlebot's speed and direction, an
     3. To launch the turtlebot Gazebo simulation, add 'rviz:=True' to the roslaunch command.
 2. To move the turtle, call the service 'resume' ('rosservice call /resume').
 3. To pause the turtle movement, call the service 'pause' ('rosservice call /pause').
-
-
-
-5. To change the parameters 'W' (the width of the figure eight), 'H' (the height of the figure eight), 'T' (the period of time it takes the turtlebot to complete the figure eight), edit the file 'config/trajectory.yaml'
-6. To change the parameter R (the frequency at which we publish the cmd_vel messages) edit the file 'launch/figure_eight.launch'.
+4. To change the parameters 'W' (the width of the figure eight), 'H' (the height of the figure eight), 'T' (the period of time it takes the turtlebot to complete the figure eight), edit the file 'config/trajectory.yaml'
+5. To change the parameter R (the frequency at which we publish the cmd_vel messages) edit the file 'launch/figure_eight.launch'.
+6. To test the calculated values at 't=0' and at 't=T/2' (half a cycle), run 'catkin_make run_tests' from the root of workspace.
--- test/test_values.py
+++ test/test_values.py
@@ -2,18 +2,24 @@
 #!/usr/bin/env python3
 
 """ Unit testing add_two """
+import rospy
+import numpy
 import unittest
 from homework2.calculations import FigureEight # TODO 
 
 class TestValues(unittest.TestCase):
-    W = rospy.get_param("parameters/width")    # initializing the width of the figure eight
-    H = rospy.get_param("parameters/height")   # initializing the height of the figure eight
-    T = rospy.get_param("parameters/period")   # initializing the amount of time it takes to complete the figure eight
-    R = rospy.get_param("~pub_freq")           # initializing the frequency at which to publish the messages
-    figure_eight = FigureEight(T, H, W)
+    def __init__(self, *args, **kwargs):
+        super(TestValues, self).__init__(*args, **kwargs)
+        W = rospy.get_param("parameters/width")    # initializing the width of the figure eight
+        H = rospy.get_param("parameters/height")   # initializing the height of the figure eight
+        self.T = rospy.get_param("parameters/period")   # initializing the amount of time it takes to complete the figure eight
+        self.figure_eight = FigureEight(self.T, H, W)
 
-    def test_equal_values(self):
-        self.assertEquals(figure_eight.test_output(0), (0, 0, 1.57079632679490, 1.74532925199433, 0, 0, 2.34810035946921, 0))
+    def test_equal_values_on_init(self):
+        numpy.testing.assert_almost_equal(self.figure_eight.test_output(0), (0, 0, 1.57079632679490, 1.74532925199433, 0, 0, 2.34810035946921, 0), decimal = 2)
+
+    def test_equal_values_after_half_cycle(self):
+        numpy.testing.assert_almost_equal(self.figure_eight.test_output(self.T/2), (0, 0, -1.57079632679490, 1.74532925199433, 0, 0, 2.34810035946921, 0), decimal = 2)
 
 if __name__ == "__main__":
     import rosunit
